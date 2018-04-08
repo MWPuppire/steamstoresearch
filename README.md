fb2c560bf968fc9ebd6b9be1207d878a04ff9189
Benjamin Kelley <mwpuppire@gmail.com>
2018-04-08 12:17:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Steam Store Search
+
+SSS (Steam Store Search) is a command line utility to search Steam's store and output results.
+
+It can be used in 2 ways. You can either type "steamstoresearch" to initiate a prompt asking you for what you want to search, or you can type a phrase after the command to search for that phrase instead of prompting.
+
+You can install it with `
+npm install -g steamstoresearch
+`
---
+++ index.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+var SteamStore = require('steam-store'),
+  store = new SteamStore({
+    country: "US",
+    language: "en"
+  }),
+  readline = require('readline-sync'),
+  htmlToText = require('html-to-text'),
+  yn = require('yn'),
+  argv = require('minimist')(process.argv.slice(2));
+
+function main() {
+    var searchterm = readline.question("Search for what? ");
+
+    store.steam('storeSearch', searchterm).then(function(results) {
+        results = results.map(function(result) {
+            return result.id;
+        });
+        store.getProductsDetails(results).then(function(details) {
+            for (var i = 0; i < details.length; i++) {
+                console.log(details[i].name);
+                console.log(htmlToText.fromString(details[i].detailed_description));
+                console.log(details[i].website);
+                var price = String(details[i].price_overview.final);
+                price = price.substring(0, price.length - 2) + "." + price.substring(price.length - 2);
+                price = details[i].price_overview.currency + " " + price;
+                if (details[i].price_overview.discount_percent != 0) {
+                    price = price + "\n" + String(details[i].price_overview.discount_percent) + "% off!";
+                };
+                console.log(price);
+                console.log("\n");
+            };
+            if (yn(readline.question("Would you like to search again? "), {default: false, lenient: true})) {
+                main();
+            };
+        });
+    });
+}
+if (argv._.length == 0) {
+    main();
+} else {
+    var searchterm = argv._.join(" ");
+    store.steam('storeSearch', searchterm).then(function(results) {
+        results = results.map(function(result) {
+            return result.id;
+        });
+        store.getProductsDetails(results).then(function(details) {
+            for (var i = 0; i < details.length; i++) {
+                if (!argv.raw) {
+                    console.log(details[i].name);
+                    console.log(htmlToText.fromString(details[i].detailed_description));
+                    console.log(details[i].website);
+                    var price = String(details[i].price_overview.final);
+                    price = price.substring(0, price.length - 2) + "." + price.substring(price.length - 2);
+                    price = details[i].price_overview.currency + " " + price;
+                    if (details[i].price_overview.discount_percent != 0) {
+                        price = price + "\n" + String(details[i].price_overview.discount_percent);
+                    };
+                    console.log(price);
+                    console.log("\n");
+                } else {
+                    console.log(details[i]);
+                };
+            };
+        });
+    });
+};
